2714e4f34a2764035a364e3ee1f268356497dd81
alparslandev <alp.develioglu@gmail.com>
2019-03-09 03:31:27 +0300

    Google sign in is added

--- app/src/main/java/com/diahelp/login/LoginActivity.kt
+++ app/src/main/java/com/diahelp/login/LoginActivity.kt
@@ -1,11 +1,31 @@
 package com.diahelp.login
 
+import android.content.Intent
 import android.os.Bundle
-import com.diahelp.base.BaseActivity
+import android.util.Log
+import android.view.View
+import com.diahelp.MainActivity
 import com.diahelp.R
+import com.diahelp.base.BaseActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity<LoginContract.View, LoginContract.Presenter>(), LoginContract.View{
 
+    companion object {
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
+    }
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun showLoading(visibility: Int) {
 
@@ -17,7 +37,95 @@ class LoginActivity : BaseActivity<LoginContract.View, LoginContract.Presenter>(
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        signInButton.setOnClickListener {signIn()}
+        signOutButton.setOnClickListener{signOut()}
+        disconnectButton.setOnClickListener{revokeAccess()}
 
         mPresenter.loginWithCredientials("", "")
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                updateUI(null)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.id!!)
+        showLoading(View.VISIBLE)
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    //Snackbar.make(main_layout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+                showLoading(View.GONE)
+            }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        googleSignInClient.signOut().addOnCompleteListener(this) {
+            updateUI(null)
+        }
+    }
+
+    private fun revokeAccess() {
+        auth.signOut()
+        googleSignInClient.revokeAccess().addOnCompleteListener(this) {
+            updateUI(null)
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        showLoading(View.GONE)
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            status.setText(R.string.signed_out)
+            detail.text = null
+            signInButton.visibility = View.VISIBLE
+            signOutAndDisconnect.visibility = View.GONE
+        }
+
     }
 }
--- app/src/main/java/com/diahelp/login/LoginPresenter.kt
+++ app/src/main/java/com/diahelp/login/LoginPresenter.kt
@@ -4,6 +4,6 @@ import com.diahelp.base.BasePresenterImpl
 
 class LoginPresenter : BasePresenterImpl<LoginContract.View>(), LoginContract.Presenter {
     override fun loginWithCredientials(email : String, pass : String) {
-        TODO("Firebase Api not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 }
