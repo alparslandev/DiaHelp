d3a897e63384f0cc5ab29f9651a2cff9b9acf0b2
alparslandev <alp.develioglu@gmail.com>
2019-03-10 14:52:21 +0300

    DayView completed

--- app/src/main/java/com/diahelp/MainActivity.kt
+++ app/src/main/java/com/diahelp/MainActivity.kt
@@ -2,11 +2,23 @@ package com.diahelp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.diahelp.ui.DayView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dv_date.onClickListener(object : DayView.Listener {
+            override fun onClickBackward(date: Calendar) {
+
+            }
+
+            override fun onClickForward(date: Calendar) {
+
+            }})
     }
 }
--- app/src/main/java/com/diahelp/ui/DayView.kt
+++ app/src/main/java/com/diahelp/ui/DayView.kt
@@ -13,8 +13,9 @@ import java.util.*
 class DayView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null,
                                         defStyleAttr: Int = 0):  LinearLayout(context, attrs, defStyleAttr) {
 
-    var date = Calendar.getInstance()
-    val sdf = SimpleDateFormat("dd MMM EEE", Locale.getDefault())
+    private var date = Calendar.getInstance()
+    private val sdf = SimpleDateFormat("dd MMM EEE", Locale.getDefault())
+    private lateinit var onClickListener : Listener
 
     init {
         LayoutInflater.from(context).inflate(R.layout.day_view, this, true)
@@ -22,24 +23,47 @@ class DayView @JvmOverloads constructor(context: Context, attrs: AttributeSet? =
 
         txt_date.gravity = Gravity.CENTER
         img_button_forward.isEnabled = false
-
-        txt_date.text = sdf.format(Date(date.timeInMillis))
+        refreshDate()
 
         img_button_backward.setOnClickListener {
+            date.add(Calendar.DAY_OF_YEAR, -1)
             if (!img_button_forward.isEnabled) img_button_forward.isEnabled = true
-
+            refreshDate()
+            onClickListener.onClickBackward(date)
         }
 
         img_button_forward.setOnClickListener {
-
+            date.add(Calendar.DAY_OF_YEAR, 1)
+            if (isToday()) img_button_forward.isEnabled = false
+            refreshDate()
+            onClickListener.onClickForward(date)
         }
+    }
+
+    fun isToday () : Boolean {
+        val today = Calendar.getInstance()
+        return (date.get(Calendar.DAY_OF_YEAR) == today.get(Calendar.DAY_OF_YEAR)
+                && date.get(Calendar.YEAR) == today.get(Calendar.YEAR))
+    }
+
+    private fun refreshDate() {
+        txt_date.text = sdf.format(Date(date.timeInMillis))
     }
 
     fun getDateStr() : String {
         return txt_date.toString()
     }
 
-    /*fun getDate() : Calendar {
+    fun getDateCalendar() : Calendar {
+        return date
+    }
 
-    }*/
+    fun onClickListener(listener : Listener) {
+        onClickListener = listener
+    }
+
+    interface Listener {
+        fun onClickForward(date : Calendar)
+        fun onClickBackward(date : Calendar)
+    }
 }
