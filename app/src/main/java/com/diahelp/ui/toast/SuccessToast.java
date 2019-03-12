98466487981823c63d02aa228556ba58d82a34d4
alparslandev <alp.develioglu@gmail.com>
2019-03-13 00:41:18 +0300

    CustomToast files are added

---
+++ app/src/main/java/com/diahelp/ui/toast/SuccessToast.java
@@ -0,0 +1,38 @@
+package com.diahelp.ui.toast;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.diahelp.R;
+
+public class SuccessToast extends Fragment {
+    LayoutInflater inflater;
+    Activity activity;
+    View view;
+    TextView title;
+    Toast toast;
+
+    public SuccessToast() {
+    }
+
+    public SuccessToast(Activity activity) {
+        this.activity = activity;
+        inflater = activity.getLayoutInflater();
+        view = inflater.inflate(R.layout.success_toast, null);
+        title = (TextView) view.findViewById(R.id.txt_custom_success_toast_message);
+        toast = new Toast(activity);
+        toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 250);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(view);
+
+    }
+
+    public void showToast(String text) {
+        title.setText(text);
+        toast.show();
+    }
+}
